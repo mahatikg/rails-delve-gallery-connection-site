f8054393489c5ce2bdae3fb5c8d67149d55a83ab
Mahati Gollamudi <github email address>
2016-08-27 16:21:25 -0400

    All view connections and routes correctly set

--- delveapp/app/controllers/museums_controller.rb
+++ delveapp/app/controllers/museums_controller.rb
@@ -1,24 +1,20 @@
 class MuseumsController < ApplicationController
-
-  #before_action :set_museum, only: [:show]
+before_action :set_museum, only: [:show]
 
   def index
     @museums = Museum.all
   end
 
   def show
-    #binding.pry
-    @museum = Museum.find(params[:id])
+    @museum = Museum.find_by(id: params[:id])
+    @galleries = Gallery.all
   end
 
   private
-    # def set_museum
-    #   @attraction = Attraction.find(params[:id])
-    # end
 
-    def museum_params
-      params.require(:museum).permit(:name, :city)
-    end
-#   params
-# => <ActionController::Parameters {"controller"=>"museums", "action"=>"show", "format"=>"2"} permitted: fals
+  def set_museum
+    @museum = Museum.find(params[:id])
+  end
+
+
 end
